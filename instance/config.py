b3ccca5aa773906162a9b1f35fad8df40d94167a
Charity Marani <marani.charity95@gmail.com>
2018-10-25 05:34:13 +0300

    Update config.py

--- instance/config.py
+++ instance/config.py
@@ -13,6 +13,7 @@ class Development(Config):
     '''Configurations for development'''
     DEBUG = True
     CONNECTION_STRING = "dbname = 'store_manager' host = 'localhost' user = 'postgres'"
+    os.environ["ENV"] = 'development'
 
 
 class Testing(Config):
@@ -20,6 +21,7 @@ class Testing(Config):
     TESTING = True
     DEBUG = True
     CONNECTION_STRING = "dbname = 'test_store_db' host = 'localhost' user = 'postgres'"
+    os.environ["ENV"] = 'testing'
 
 
 app_config = {
